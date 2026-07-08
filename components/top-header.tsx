f81aed8e15db30972a3bfd62ffb11dd8cf3df6b6
WarData <45937614+WarData@users.noreply.github.com>
2026-07-08 21:04:00 +0200

    Update top-header.tsx

--- components/top-header.tsx
+++ components/top-header.tsx
@@ -7,7 +7,7 @@ export default function TopHeader() {
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:px-6">
         <Link href="/" className="wm-brand">
           <Image
-  src="/Winmotor-Y-MS.jpg"
+  src="/Winmotor-Y-MS.jpg?v=2"
   alt="Winmotor Y-MS"
   width={340}
   height={80}
